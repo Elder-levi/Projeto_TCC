87ec8ddb53e5e277e02160674b8d20629701a875
Levi <elderlevisantos@gmail.com>
2025-05-22 10:26:23 -0300

    Adicionado

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,7 +9,8 @@ const routes = [
   // rota “catch-all” para 404 (opcional):
   { path: '/:pathMatch(.*)*', redirect: '/' }
 ]
-
+ // Testes
+ 
 const router = createRouter({
   history: createWebHistory(), 
   routes,
